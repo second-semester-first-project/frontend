2166adb3dfb18e5718b191de1e7f645fd97ff3bc
dldbsgh00005 <dldbsgh00005@gmail.com>
2025-01-12 20:04:14 +0900

    create : project

---
+++ frontend/README.md
@@ -0,0 +1,3 @@
+# REACT APP README.md FILE
+
+리액트 앱 리드미 파일!!!!
---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div className="">
+      <header className="">
+        <h1> second-semester-first-project </h1> <br />
+        <h2> AIoT Team Page </h2>
+      </header>
+    </div>
+  );
+}
+
+export default App;
